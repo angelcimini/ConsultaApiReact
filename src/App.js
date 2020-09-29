c2a92b617fc44e756f67f6499d89729ab599ec89
angelcimini <66746746+angelcimini@users.noreply.github.com>
2020-09-29 05:26:37 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import styled from '@emotion/styled';
+import Frase from './components/Frase';
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left, darkgreen 0%, forestgreen 40%, green 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: bold;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  border-radius: 0.5em;
+  transition: background-size .8s ease;
+  :hover {
+    cursor: pointer;
+    background-size: 400px;
+  }
+`;
+
+function App() {
+
+  const [frase, setfrase] = useState({});
+
+  const consultarAPI = async () => {
+    const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    const frase = await api.json();
+    setfrase(frase[0]);
+  }
+
+  // const consultarAPI = () => {
+  //   const api = fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+  //   const frase = api.then( respuesta => respuesta.json() );
+  //   frase.then( resultado => console.log(resultado) )
+  // }
+
+  useEffect( () => {
+    consultarAPI();
+  }, []); 
+
+  return (
+    <Contenedor>
+      <Frase
+        frase={frase}
+      />
+      <Boton
+        onClick={consultarAPI}
+      >
+        Obtener frase
+      </Boton>
+    </Contenedor>
+  );
+}
+
+export default App;
